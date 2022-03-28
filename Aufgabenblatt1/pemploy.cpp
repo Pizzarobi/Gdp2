8af51894f4105ca427cc91b95c4f4c190e1dd936
Robert Kalmar <24878451+Pizzarobi@users.noreply.github.com>
2022-03-28 15:31:18 +0200

    28.03.2022
    Added some functions

--- Aufgabenblatt1/pemploy.cpp
+++ Aufgabenblatt1/pemploy.cpp
@@ -1,30 +1,57 @@
 #include <iostream>
 #include <vector>
+#include <ctime>
 using namespace std;
 
 
-void print(string name, string firstName, vector<int> dateOfBirth, float salary){
+void print(const string& name, const string& firstName, const vector<int>& dateOfBirth, const float& salary){
+    cout.setf(ios::fixed);
+    cout.precision(2);
     cout << "Name:       " << name << "\n";
-    cout << "Vorname:    " << firstName << "\n";
-    cout << "Geburtstag: " << dateOfBirth[2] << "."<< dateOfBirth[1] << "." << dateOfBirth[0] << "\n";
+    cout << "Vorname:    " << firstName << "\n" << "Geburtstag: ";
+    cout.width(2); cout.fill('0');
+    cout << dateOfBirth[2] << ".";
+    cout.width(2); cout.fill('0');
+    cout << dateOfBirth[1] << "." << dateOfBirth[0] << "\n";
     cout << "Gehalt:     " << salary << " Euro\n";
+}
 
+int age(const vector<int> dateOfBirth){
+    // Get current time
+    time_t now = time(nullptr);
+    tm * t = localtime(&now);
+    int year = t->tm_year + 1900;
+    int month = t->tm_mon + 1;
+    int day = t->tm_mday;
+
+    int age_Y = year - dateOfBirth[0];
+    int age_M = month - dateOfBirth[1];
+    int age_D = day - dateOfBirth[2];
+
+    if(age_M <= 0)
+        if(age_D < 0)
+            age_Y--;
+
+    return age_Y;
+}
+
+void riseSalary(float& salary, const float rise){
+    salary *= 1+rise;
 }
 
 
 int main(){
-    cout.width(2);
-    cout.fill('0');
-    cout.setf(ios::fixed);
-    cout.precision(2);
 
-    string name = "Hentai";
-    string firstName = "Steven";
+    string firstName = "Yo";
+    string name = "Momma";
 
-    vector<int> dateOfBirth = {1969, 9, 6};
-    float salary = 2.0;
+    vector<int> dateOfBirth = {2001,3, 2};
+    float salary = 100.0;
+
+    riseSalary(salary,0.5);
 
     print(name, firstName, dateOfBirth, salary);
+    printf("Age:        %d\n",age(dateOfBirth));
 
     return 0;
 }
